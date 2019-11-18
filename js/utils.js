375231c7d91c8495a386b8c555e18742fa0bac56
123 <>
2019-11-18 14:16:14 +0300

    no message

--- js/utils.js
+++ js/utils.js
@@ -19,6 +19,11 @@
 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(24840335, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true });
 
 !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?162",t.onload=function(){VK.Retargeting.Init("VK-RTRG-412502-cCwct"),VK.Retargeting.Hit()},document.head.appendChild(t)}();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-57887378-1', 'auto', {'allowLinker': true});
+ga('require', 'linker');
+ga('linker:autoLink', ['cloud.roistat.com'] );
+ga('send', 'pageview');
 
 (function ($) {
     var initModal = function (el, options) {
@@ -162,9 +167,12 @@
             //     }
             // });
 
-
             var url = "https://cloud.roistat.com/lead/register";
             var urlWithParams = url + "?" + 'email=' + $emailField.val() + '&name=' + $nameField.val() + '&roistat_id=' + getCookie('roistat_visit') + '&phone=' + $phoneField.val() + '&public_key=' + public_key + '&is_need_response=1';
+
+            fbq('track', 'Lead');
+            ga('send', 'event', 'roistat_lead', 'click');
+
             window.location.href = urlWithParams;
         }
 
@@ -280,7 +288,4 @@
         // });
         initModal($(this), options);
     };
-
-    fbq('track', 'Lead');
-    ga('send', 'event', 'roistat_lead', 'click');
 })(jQuery);
